fbd9b9245fb64febc2a6826b4a9706002a083a53
salesfordev <salesfordev@gmx.fr>
2020-09-21 16:40:51 +0200

    Commentaire sur quelques modifs à venir

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,33 +3,36 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.Map;		//- Importation des bibliothèques necessaires : "java.util.Map"
+import java.util.TreeMap;	//- java.util.TreeMap"
 
 public class AnalyticsCounter {
 	
 		
-		//- Importation des bibliothèques necessaires "java.util.Map, java.util.TreeMap"
-		private static Map<String,Integer> counter = new TreeMap<>(); //-	Création d’un nouvel objet de type TreeMap nomé "counter"
+		
+		private static Map<String,Integer> counter = new TreeMap<>(); //-Création d’un nouvel objet de type TreeMap nomé "counter"
 	 	
 		public static void main(String args[]) throws Exception {
+			
 		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("Project02Eclipse/symptoms.txt"));
-		String line = reader.readLine();
+			
+		// Création d'un objet reader(BufferedReader) / Mise en mémoire Tampon du fichier symptoms.txt
+		BufferedReader reader = new BufferedReader (new FileReader("Project02Eclipse/symptoms.txt"));//-Rectification chemin d'accès au fichier-l'appli ne se lance pas.
+		String line = reader.readLine(); // Lecture ligne par ligne 
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
+		// int i = 0;	// set i to 0   ----commentaire inutile--- comme on utilise une boucle while, je suprime cette ligne.
+		int headCount = 0;	// counts headaches --- supression de cette initialisation, les symptomes seront lus automatiquement dans le fichier (symptoms.txt)
 		while (line != null) {
-			i++;	// increment i
+			i++;	// increment i 											  // cette ligne sera supprimée
 			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
+			if (line.equals("headache")) { 								      // Cette ligne sera suprimée
+				headCount++; 												  // Cette ligne sera suprimée
+				System.out.println("number of headaches: " + headCount); 	  // Cette ligne sera suprimée
 			}
-			else if (line.equals("rush")) {
+			else if (line.equals("rush")) {								 	  // Cette ligne sera suprimée
 				rashCount++;
 			}
-			else if (line.contains("pupils")) {
+			else if (line.contains("pupils")) {							  	  // Cette ligne sera suprimée
 				pupilCount++;
 			}
 
