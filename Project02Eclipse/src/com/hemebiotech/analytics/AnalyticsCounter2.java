d90a935234bbcba941f24d4f9ad9dd30dd9dd77b
salesfordev <salesfordev@gmx.fr>
2020-09-27 17:25:15 +0200

    mise à jour

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter2.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter2.java
@@ -10,10 +10,8 @@ public class AnalyticsCounter2 {
 	public static void main(String args[]) throws Exception {
 		
 		ISymptomReader reader = new ReadSymptomDataFromFile(); 
-		
-		Map<String, Integer> symptoms = reader.readSymptomData();// Lecture des symptome depuis le fichier.
-		
 		ISymptomWriter writer = new WriteSymptomDataToFile();
+		Map<String, Integer> symptoms = reader.readSymptomData();// Lecture des symptome depuis le fichier.
 		writer.writeSymptoms(symptoms);// Ecriture des symptômes dans le fichier résult.out.
 		
 		
